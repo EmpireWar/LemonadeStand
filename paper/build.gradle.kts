6075ce731938f7399e05d7abaa751126792ce63e
SamB440 <sam@islandearth.net>
2024-05-07 12:44:38 +0100

    Setup multi-module workspace

---
+++ paper/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("buildlogic.java-common-conventions")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
+    implementation(project(":common"))
+}
+
+tasks {
+    processResources {
+        filesMatching("plugin.yml") {
+            expand("version" to version)
+        }
+    }
+}
