eaae7ee940c0a5943c98a90de8b386c1e5501ba5
SamB440 <sam@islandearth.net>
2024-09-23 15:55:25 +0100

    Fix relocation

--- build-logic/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
@@ -31,8 +31,12 @@ repositories {
 project.version = "1.0.1-SNAPSHOT"
 
 dependencies {
-    implementation("io.javalin:javalin:6.2.0")
-    implementation("club.minnced:discord-webhooks:0.8.2")
+    implementation("io.javalin:javalin:6.2.0") {
+        exclude("org.slf4j")
+    }
+    implementation("club.minnced:discord-webhooks:0.8.2") {
+        exclude("org.slf4j")
+    }
     compileOnly("org.slf4j:slf4j-api:2.0.12")
     testImplementation("org.slf4j:slf4j-api:2.0.12")
     compileOnly("com.google.code.gson:gson:2.10.1")
@@ -68,7 +72,19 @@ tasks {
     }
 
     shadowJar {
+        mergeServiceFiles()
         archiveBaseName.set("lemonadestand-${project.name}")
         archiveClassifier.set("")
+        relocate("club.minnced", "lemonadestand.libs.discord")
+        relocate("io.javalin", "lemonadestand.libs.javalin")
+        relocate("jakarta.servlet", "lemonadestand.libs.jakarta")
+        relocate("javax.servlet", "lemonadestand.libs.javax")
+        relocate("kotlin", "lemonadestand.libs.kotlin")
+        relocate("okhttp3", "lemonadestand.libs.okhttp3")
+        relocate("okio", "lemonadestand.libs.okio")
+        relocate("org.eclipse.jetty", "lemonadestand.libs.jetty")
+        relocate("org.intellij.lang.annotations", "lemonadestand.libs.annotations")
+        relocate("org.jetbrains.annotations", "lemonadestand.libs.jannotations")
+        relocate("org.json", "lemonadestand.libs.json")
     }
 }
